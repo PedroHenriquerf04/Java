9e3791a159b1ab94cf43327193e634f579e5f4ec
Pedro Henrique <138182328+PedroHenriquerf04@users.noreply.github.com>
2024-05-19 14:20:55 -0400

    Add files via upload

---
+++ Estruturas de Repetição/EstruturaRepeticaoParte2.java
@@ -0,0 +1,17 @@
+package java_basico;
+
+public class EstruturaRepeticaoParte2 {
+    public static void main(String[] args) {
+        /* Estrutura Do-While
+        do{
+            comandos;
+        }while(teste logico);
+         */
+
+        int num = 0;
+        do {
+            System.out.println(num);
+            num++;
+        }while (num <= 10);
+    }
+}
---
+++ Estruturas de Repetição/EstruturaRepeticaoParte3.java
@@ -0,0 +1,15 @@
+package java_basico;
+
+public class EstruturaRepeticaoParte3 {
+    public static void main(String[] args) {
+        /* estrutura com variavel de controle
+        for(int var = valor inicial; valor final; incremento){
+            comandos;
+        }
+         */
+
+        for (int num = 0; num <= 100; num+=10){
+            System.out.print(num +" ");
+        }
+    }
+}
